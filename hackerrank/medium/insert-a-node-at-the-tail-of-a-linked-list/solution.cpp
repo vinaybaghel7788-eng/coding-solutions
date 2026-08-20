81d59e05dc5d0fce9e878a09349b62fd1fd76a8f
vinaybaghel7788-eng <vinaybaghel7788@gmail.com>
2026-08-20 23:04:41 +0530

    ✅ Insert a Node at the Tail of a Linked List | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-20

---
+++ hackerrank/medium/insert-a-node-at-the-tail-of-a-linked-list/solution.cpp
@@ -0,0 +1,32 @@
+
+
+// Complete the insertNodeAtTail function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtTail(SinglyLinkedListNode* head, int data) {
+    SinglyLinkedListNode* newNode = new SinglyLinkedListNode(data);
+    
+    if(head == nullptr) {
+        return newNode;
+    }
+    
+    SinglyLinkedListNode*temp = head;
+    
+    while(temp->next != nullptr) {
+        temp = temp->next;
+    }
+    
+    temp->next = newNode;
+    return head;
+
+
+}
+
